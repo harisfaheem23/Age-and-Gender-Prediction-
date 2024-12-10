8f5f6f85ac349de2a130330f6aa643779d5c861b
harisfaheem23 <muhammadharis18feb205@gmail.com>
2024-12-10 15:29:14 +0500

    Update ageandgender.py

--- ageandgender.py
+++ ageandgender.py
@@ -26,7 +26,7 @@ st.sidebar.info(
     2. The model processes the image and predicts the results.
     
     Developed by: **Haris Faheem and Hassan Nasir**
-    Contact: [LinkedIn Profile](https://www.linkedin.com/in/haris-faheem-1376982a3/)'''
+    \nContact: [LinkedIn Profile](https://www.linkedin.com/in/haris-faheem-1376982a3/)'''
     
 )
 
